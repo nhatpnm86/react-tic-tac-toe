5ef2d0aca498aaa1c5d04c6c4c285ede078e0291
Nguyen Minh Nhat Pham <phamngu@deakin.edu.au>
2019-11-12 22:53:25 +1100

    When no one wins, display a message about the result being a draw

--- src/index.js
+++ src/index.js
@@ -128,6 +128,8 @@ class Game extends React.Component {
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
+    } else if (current.squares.every((value) => value)) {
+      status = 'Draw!';
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
